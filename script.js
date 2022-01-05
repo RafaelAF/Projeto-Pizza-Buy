d437f7549374013c036abea8da7a03bf41eb2430
Antonio Rafael <andrade.rafa71@gmail.com>
2022-01-05 18:35:44 -0300

    Projeto compra de pizza b7Web

---
+++ script.js
@@ -0,0 +1,58 @@
+let modalQt = 1;
+
+
+
+const c = (el)=> document.querySelector(el);
+const cs = (el)=> document.querySelectorAll(el);
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    // preencher as informações
+
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault(); // Prevenir a atualização padrão da tela
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+        //console.log(pizzaJson[key].name)
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaWindowArea .pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaWindowArea .pizzaInfo .pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaWindowArea .pizzaInfo .pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        cs('.pizzaInfo--size').forEach((size, index)=>{
+            if(index == 2){
+                size.classList.add('selected')
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[index];
+        })
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=> {
+            c('.pizzaWindowArea').style.opacity = 1
+        }, 200)
+        
+
+    })
+
+
+    c('.pizza-area').append( pizzaItem);
+});
+
+//EFEITOS DO MODAL
+const closeModal = ()=>{
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500)
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal)
+})
